c7231fe1b481f4b9274f8165c1a55038791f8b9a
Pantaleón Ferrer <pantaferrer@hotmail.com>
2025-03-10 19:22:22 +0100

    Update dataSourceConfig.ts

--- src/dataSourceConfig.ts
+++ src/dataSourceConfig.ts
@@ -1,18 +1,23 @@
-import 'dotenv/config'
+import "dotenv/config";
 import { DataSource, DataSourceOptions } from "typeorm";
 
 const dataSource = new DataSource({
-    host: process.env.DB_HOST,
-    port: Number(process.env.DB_PORT),
-    username: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_NAME,
-    synchronize: false,
-    logging: false,
-    ssl: true,
-    entities: [__dirname + '/**/*.schema.{ts,js}'],
-    migrations: ['persistence/*.js'],
-    type: 'postgres',
-} as DataSourceOptions)
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT),
+  username: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+  synchronize: false,
+  logging: false,
+  ssl: true,
+  extra: {
+    ssl: {
+      rejectUnauthorized: false,
+    },
+  },
+  entities: [__dirname + "/**/*.schema.{ts,js}"],
+  migrations: ["persistence/*.js"],
+  type: "postgres",
+} as DataSourceOptions);
 
-export default dataSource
+export default dataSource;
